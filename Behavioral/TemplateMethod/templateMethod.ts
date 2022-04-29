b940d0d35390b6a3085322d94204798e9f51e1d0
igor <ibrownramos@gmail.com>
2022-04-29 00:55:47 -0300

    feat: create Behavioral folder and first pattern: TemplateMethod

---
+++ Behavioral/TemplateMethod/templateMethod.ts
@@ -0,0 +1,32 @@
+ abstract class TemplateMethodBaseClass {
+    
+    // Como não existe método final em js/ts, um hack seria declara-lo como READONLY para que ele não possa ser sobrescrito
+    readonly templateMethod = (): void => {
+        this.stepA();
+        this.hook();
+        this.stepB();
+    }
+
+    abstract stepA(): void;
+    abstract stepB(): void;
+    hook(): void {}
+}
+
+export class ConcreteTemplateMethod extends TemplateMethodBaseClass {
+    stepA(): void {
+        console.log('A - stepA')
+    }
+    
+    stepB(): void {
+        console.log('A - stepB')
+    }
+
+    hook(): void {
+        console.log('A - Hook foi usado')
+    }
+}
+
+// Client
+
+const use = new ConcreteTemplateMethod();
+use.templateMethod();
